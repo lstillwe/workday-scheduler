e0fa8d3c30ac9a7e10b7a5c46d2dcd8052e0c2cc
Lisa Stillwell <lisa@renci.org>
2021-11-12 17:46:13 -0500

    #3 Added functionality to save tasks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,5 @@
-// retrieve any needed paraent elements
-var containerEl = document.querySelector(".container");
-
-// textarea base classes 
-textAreaClasses = "col-md-10 description";
-
-// get todays date and format it like this: Monday, January 1, 2021
-var currentDate = moment().format('dddd[,] MMMM Do[,] YYYY')
-var dateParagraphEl = document.querySelector("#currentDay");
-dateParagraphEl.textContent = currentDate;
-
-// create blocks 
-var timeBlockEl = document.createElement("div");
-timeBlockEl.setAttribute("class", "time-block");
-containerEl.append(timeBlockEl);
-
-// add hour blocks 9AM - 5PM  (9 blocks)
-var startingHour = 9;
-var endingHour = 17;
-for (var i=startingHour; i<=endingHour; i++) {
-   var hourBlock = createHourBlock(i);
-   timeBlockEl.append(hourBlock);
-}
-
-containerEl.append(timeBlockEl);
-
-// run checkTime for first time
-checkTime();
-
-// start the time monitoring - check every minute
-var intervalID = setInterval(checkTime, 60000);
-
-function createHourBlock(index) {
+// function to create the main content in the time block container
+var createHourBlock = function(index, task) {
     // create div
     var hourBlockEl = document.createElement("section");
     hourBlockEl.setAttribute("class", "time-block");
@@ -47,6 +16,9 @@ function createHourBlock(index) {
     // create textarea for task description
     var textAreaEl = document.createElement("textarea");
     textAreaEl.setAttribute("id", index);
+    if (task) {
+        textAreaEl.value = task.text;
+    }
 
     // create submit button
     var btnEl = document.createElement("button");
@@ -68,7 +40,9 @@ function createHourBlock(index) {
     return hourBlockEl;
 }
 
-function checkTime() {
+// called every minute to check the current time and 
+// update the hour block colors
+var checkTime = function() {
     // get current hour
     var currentTime = moment().hour();
 
@@ -99,3 +73,91 @@ function checkTime() {
         }
     }
  }
+
+ // retrieve any saved tasks in local storage
+ var getTasks = function() {
+    
+    var tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    return tasks;
+ }
+
+ // save current task to local storage
+ var saveTask = function(event) {
+
+    // retrieve currently saved tasks, if any
+    var savedTasks = getTasks();
+    if (!savedTasks) {
+        savedTasks = [];
+    }
+
+    // this element's parent
+    var parentEl = event.currentTarget.parentElement;
+    // now find textarea associated with this parent
+    var textareaEl = parentEl.querySelector('textarea');
+    newTask = textareaEl.value.trim();
+
+    // see if this element has currently saved task
+    var item = savedTasks.find(x => x.hour == textareaEl.id);
+    // if already saved task - just update it with text
+    if (item) {
+        item.text = newTask;
+    }
+    // otherwise add this task to the arrray and save
+     else {
+        // add this task to the arrray and save
+        taskObj = {
+            "hour": parseInt(textareaEl.id),
+            "text": newTask
+        }
+        savedTasks.push(taskObj);
+    }
+
+    // save modified tasklist to local storage
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+}
+
+
+// *************** MAIN *************** //
+
+// retrieve any needed paraent elements
+var containerEl = document.querySelector(".container");
+
+// textarea base classes 
+textAreaClasses = "col-md-10 description";
+
+// get todays date and format it like this: Monday, January 1, 2021
+var currentDate = moment().format('dddd[,] MMMM Do[,] YYYY')
+var dateParagraphEl = document.querySelector("#currentDay");
+dateParagraphEl.textContent = currentDate;
+
+// create blocks 
+var timeBlockEl = document.createElement("div");
+timeBlockEl.setAttribute("class", "time-block");
+containerEl.append(timeBlockEl);
+
+// add hour blocks 9AM - 5PM  (9 blocks)
+var startingHour = 9;
+var endingHour = 17;
+var startupTasks = getTasks();
+var task = null;
+for (var i=startingHour; i<=endingHour; i++) {
+    // find saved task description for this item, if anny
+    if (startupTasks) {
+        task = startupTasks.find(x => x.hour == i);
+    }
+   var hourBlock = createHourBlock(i, task);
+   timeBlockEl.append(hourBlock);
+}
+
+containerEl.append(timeBlockEl);
+
+// run checkTime for first time
+checkTime();
+
+// add lsitener for for submit buttons
+
+$(".saveBtn").on("click", saveTask);
+
+// start the time monitoring - check every minute
+var intervalID = setInterval(checkTime, 60000);
