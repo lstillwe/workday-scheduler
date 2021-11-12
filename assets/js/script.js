93505fd12a8edb102448db2b92c8556cbf466c64
Lisa Stillwell <lisa@renci.org>
2021-11-12 15:27:09 -0500

    #2 add color changes for past, present, and future times

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 // retrieve any needed paraent elements
 var containerEl = document.querySelector(".container");
 
-// get todays date and format it like this: Monday, January 1, 2021
+// textarea base classes 
+textAreaClasses = "col-md-10 description";
 
+// get todays date and format it like this: Monday, January 1, 2021
 var currentDate = moment().format('dddd[,] MMMM Do[,] YYYY')
-console.log(currentDate);
 var dateParagraphEl = document.querySelector("#currentDay");
 dateParagraphEl.textContent = currentDate;
 
@@ -23,6 +24,12 @@ for (var i=startingHour; i<=endingHour; i++) {
 
 containerEl.append(timeBlockEl);
 
+// run checkTime for first time
+checkTime();
+
+// start the time monitoring - check every minute
+var intervalID = setInterval(checkTime, 60000);
+
 function createHourBlock(index) {
     // create div
     var hourBlockEl = document.createElement("section");
@@ -34,12 +41,12 @@ function createHourBlock(index) {
     // create hour div
     var hourDivEl = document.createElement("div");
     hourDivEl.setAttribute("class", "col-md-1 hour");
-    hourDivEl.setAttribute("id", index);
+    //hourDivEl.setAttribute("id", index);
     hourDivEl.textContent = moment(index, "H").format("hA");
 
     // create textarea for task description
     var textAreaEl = document.createElement("textarea");
-    textAreaEl.setAttribute("class", "col-md-10 description")
+    textAreaEl.setAttribute("id", index);
 
     // create submit button
     var btnEl = document.createElement("button");
@@ -50,12 +57,45 @@ function createHourBlock(index) {
     faIconEL.setAttribute("class", "fas fa-save");
     btnEl.append(faIconEL);
 
-
+    // add hour div, textarea, and button to row div
     rowDivEl.append(hourDivEl);
     rowDivEl.append(textAreaEl);
     rowDivEl.append(btnEl);
 
+    // add everything to the hour section
     hourBlockEl.append(rowDivEl);
 
     return hourBlockEl;
 }
+
+function checkTime() {
+    // get current hour
+    var currentTime = moment().hour();
+
+    // use the folowing for testing
+    //var currentTime = moment().add(3, 'hours').hour();
+    //var currentTime = moment().subtract(3, 'hours').hour();
+    //console.log(currentTime);
+
+    // update the current color for present hour, if in business hours
+    if (currentTime >= startingHour && currentTime <= endingHour) {
+        var presentHourBlockEl = document.getElementById(currentTime);
+        presentHourBlockEl.setAttribute("class", textAreaClasses + " present");
+    }
+
+    // find how many hours have passed since startingHour
+    var hoursPassed = currentTime - startingHour;
+    for(var i=startingHour; i<currentTime; i++) {
+        // update the pasthours with the correct colors
+        var passedHourBlockEl = document.getElementById(i);
+        passedHourBlockEl.setAttribute("class", textAreaClasses + " past");
+    }
+
+    // set the rest to future if in business hours
+    for(var i=currentTime+1; i<=endingHour; i++) {
+        if (i >= startingHour && i <= endingHour) {
+            var futureHourBlockEl = document.getElementById(i);
+            futureHourBlockEl.setAttribute("class", textAreaClasses + " future");
+        }
+    }
+ }
